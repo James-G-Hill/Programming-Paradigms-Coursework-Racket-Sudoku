cb18a198792805003472dfb66ff1a3fdc88460ad
James Hill <j.g.hill@zoho.com>
2017-02-07 22:16:59 +0000

    Added tests for the 'setReplace' function.

--- Sudoku-Test.rkt
+++ Sudoku-Test.rkt
@@ -19,3 +19,13 @@
    (list 1 2 3 4 5 6 7 8 9)
    (list 1 2 3 4 5 6 7 8 9)
    (list 1 2 3 4 5 6 7 8 9)))
+
+
+;;;
+;;;  TRANSFORM TESTING
+;;;
+
+;;; Test 'setReplace'
+(check-equal? (setReplace 0) (list 1 2 3 4 5 6 7 8 9) "Replace Zero with a List (1-9)")
+(check-equal? (setReplace 1) (list 1) "Replace 1 with a List (1)")
+(check-equal? (setReplace 9) (list 9) "Replace 1 with a List (9)")
